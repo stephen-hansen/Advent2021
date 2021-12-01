ef33d5007d06f70a5724810768d0f49e1169459a
Stephen Hansen <sph77@drexel.edu>
2021-12-01 17:56:00 -0500

    setting up

---
+++ 02/p1.py
@@ -0,0 +1,11 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils import *
+
+def main():
+    p("foobar")
+
+if __name__ == "__main__":
+    main()
+
