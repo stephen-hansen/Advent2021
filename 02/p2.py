222706b356581ae97bfdb74b13cab4a7c5b9a90a
Stephen Hansen <sph77@drexel.edu>
2021-12-02 00:04:13 -0500

    day 2

--- 02/p2.py
+++ 02/p2.py
@@ -7,16 +7,18 @@ def main():
     data = getlines_map_regex('(.*) (.*)')
     horiz = 0
     depth = 0
+    fake_depth = 0
     for item in data:
         direct = item.group(1)
         amt = int(item.group(2))
         if direct == 'forward':
             horiz += amt
+            fake_depth += depth * amt
         elif direct == 'down':
             depth += amt
         elif direct == 'up':
             depth -= amt
-    p(depth*horiz)
+    p(fake_depth*horiz)
 
 if __name__ == "__main__":
     main()
