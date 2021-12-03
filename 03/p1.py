8b6727814f6aa48491f0aebe1abcc88e0b6ffb43
Stephen Hansen <sph77@drexel.edu>
2021-12-02 21:13:05 -0500

    day 3 setup

---
+++ 03/p1.py
@@ -0,0 +1,12 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from utils import *
+
+def main():
+    A = getlines()
+    p(d)
+
+if __name__ == "__main__":
+    main()
+
