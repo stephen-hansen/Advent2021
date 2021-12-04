416f2b54fd31022a25d5d7c7050936f3e46bb375
Stephen Hansen <sph77@drexel.edu>
2021-12-03 20:21:38 -0500

    prep day 4

---
+++ 04/p1.py
@@ -0,0 +1,13 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from collections import defaultdict
+from functools import reduce
+from utils import *
+
+def main():
+    A = getlines()
+    
+if __name__ == "__main__":
+    main()
+
