d9f4fa58086daaa5e692bf8b0978655a36698522
Stephen Hansen <sph77@drexel.edu>
2021-12-04 00:19:54 -0500

    day 4, wack

--- 04/p2.py
+++ 04/p2.py
@@ -4,12 +4,12 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from collections import defaultdict
 from functools import reduce
 from utils import *
+import copy
 
 def get_sum(board, ns, n):
     un_sum = 0
     win_sum = int(n)
     for r in board[:5]:
-        print(r)
         for c in r:
             if c not in ns:
                 un_sum += int(c)
@@ -37,22 +37,29 @@ def main():
             nb.append(b[i])
         for i in range(len(b[0])):
             nb.append([x[i] for x in b])
+        nb.append(False)
         better_boards.append(nb)
     
     n_present = set()
+    last_won = None
+    last_n = None
+    last_set = None
     for n in ns:
         print(n)
         n_present.add(n)
         for b in better_boards:
-            for r in b:
+            for r in b[:-1]:
                 match = True
                 for c in r:
                     if c not in n_present:
                         match = False
                         break
-                if match:
-                    get_sum(b, n_present, n)
-                    return
+                if match and b[-1] == False:
+                    b[-1] = True
+                    last_won = copy.deepcopy(b)
+                    last_n = n
+                    last_set = copy.deepcopy(n_present)
+    get_sum(last_won, last_set, last_n)
 
 
 if __name__ == "__main__":
