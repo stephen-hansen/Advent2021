4eb34970b50d830960e65228afcd2cbe9edf8009
stephen-hansen <sph77@drexel.edu>
2021-12-05 21:08:53 -0500

    day6 setup

---
+++ 06/p1.py
@@ -0,0 +1,15 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from collections import defaultdict
+from functools import reduce
+from utils import *
+
+def main():
+    A = getlines_map_regex("")
+    for l in A:
+        pass
+
+if __name__ == "__main__":
+    main()
+
