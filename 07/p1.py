d4dddf7077674d6ebf46440042fe1d26a826a000
Stephen Hansen <sph77@drexel.edu>
2021-12-07 00:06:32 -0500

    day 7 joke

---
+++ 07/p1.py
@@ -0,0 +1,26 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from collections import defaultdict
+from functools import reduce
+from utils import *
+import copy
+
+def main():
+    A = [int(x) for x in getlines_getsv(',')[0]]
+    maxp = max(A)
+    f = 0
+    minf = 99999999
+    minv = None
+    for i in range(maxp):
+        f = 0
+        for c in A:
+            f += abs(c - i)
+        if f < minf:
+            minf = f
+            minv = i
+    p(minf)
+
+if __name__ == "__main__":
+    main()
+
