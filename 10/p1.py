5c894b0ad645146b6e92ea299606781965e974d3
stephen-hansen <sph77@drexel.edu>
2021-12-09 21:41:49 -0500

    setup

---
+++ 10/p1.py
@@ -0,0 +1,18 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from collections import defaultdict
+from functools import reduce
+from utils import *
+import copy
+
+def main():
+    A = getlines()
+    c = 0
+    for l in A:
+        pass    
+    p(c)
+
+if __name__ == "__main__":
+    main()
+
