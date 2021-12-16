9a52c39f45ea206dc1ae992339f062d1b8b65e52
stephen-hansen <sph77@drexel.edu>
2021-12-16 15:53:58 -0500

    Day 16, it depends

---
+++ 16/p1.py
@@ -0,0 +1,88 @@
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from collections import defaultdict, Counter
+from functools import reduce
+from utils import *
+import copy
+
+def parse_packet(b):
+    goal = 0
+    i = 0
+    version = int(b[0:3], 2)
+    p(f"Version = {version}")
+    goal += version
+    type_id = int(b[3:6], 2)
+    if type_id == 4:
+        p("Literal")
+        # Literal
+        lit_val = ''
+        i = 6
+        while True:
+            c = b[i]
+            i += 1
+            lit_val += b[i:i+4]
+            i += 4
+            if c == '0':
+                break
+        lit_val = int(lit_val, 2)
+    else:
+        p("Operator")
+        # Operator
+        length_type_id = b[6]
+        if length_type_id == '0':
+            # Total bit length
+            bit_length = int(b[7:22], 2)
+            i = 22
+            length = 0
+            while length < bit_length:
+                amt, prev_goal = parse_packet(b[i:])
+                goal += prev_goal
+                i += amt
+                length += amt
+        else:
+            # Number of sub-packets
+            sub_packets = int(b[7:18], 2)
+            i = 18
+            n_pkt = 0
+            while n_pkt < sub_packets:
+                amt, prev_goal = parse_packet(b[i:])
+                goal += prev_goal
+                i += amt
+                n_pkt += 1
+    return i, goal # i is length, goal is whatever it is
+
+
+def main():
+    A = getlines()
+    maps = {'0': '0000',
+            '1': '0001',
+            '2': '0010',
+            '3': '0011',
+            '4': '0100',
+            '5': '0101',
+            '6': '0110',
+            '7': '0111',
+            '8': '1000',
+            '9': '1001',
+            'A': '1010',
+            'B': '1011',
+            'C': '1100',
+            'D': '1101',
+            'E': '1110',
+            'F': '1111'}
+    binstrs = []
+    for l in A:
+        binstr = ''
+        for c in l:
+            binstr += maps[c]
+        binstrs.append(binstr)
+    goal = 0
+    for b in binstrs:
+        i, bgoal = parse_packet(b)
+        goal += bgoal
+    p(goal)
+
+if __name__ == "__main__":
+    main()
+
